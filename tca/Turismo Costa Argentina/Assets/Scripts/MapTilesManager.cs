b23c39944735b530c9ab64e7767095a1840a6660
Roberto G. Fernández <rgfernan@gmail.com>
2024-09-01 21:52:52 -0300

    Scroll infinito de carretera.

--- tca/Turismo Costa Argentina/Assets/Scripts/MapTilesManager.cs
+++ tca/Turismo Costa Argentina/Assets/Scripts/MapTilesManager.cs
@@ -8,6 +8,13 @@ public class MapTilesManager : MonoBehaviour {
     public GameObject roadTilesetPrototype;
     private Dictionary<string, GameObject> tilesDictionary;
     private Dictionary<string, GameObject> roadTilesSetDictionary;
+    private GameObject[] allTilesets;
+
+    public GameObject charactersManagerGameObject;
+    public CharactersManager charactersManager;
+
+    private float completeTileSetSize = 21f;
+    private float tileSetsAmount = 8f;
 
     public MapTilesManager()
     {
@@ -17,22 +24,86 @@ public class MapTilesManager : MonoBehaviour {
 
     void Start() 
     {
+        charactersManager = charactersManagerGameObject.GetComponent<CharactersManager>();
+
+        float initialX = -12f;
         tilesDictionary.Add("single road", singleRoadTile);
         tilesDictionary.Add("single curve road", singleCurveRoadTile);
         tilesDictionary.Add("diagonal 2x2 road", diagonal2x2RoadTile);
 
-        roadTilesSetDictionary.Add("basic straight road", createBasicStraightTileset());
         //roadTilesSetDictionary.Add("basic horizontal straight road", createHorizontalBasicStraightTileset());
-        //roadTilesSetDictionary["basic horizontal straight road"].transform.position = new Vector3(-3f, -3f, 0.2f);
+        roadTilesSetDictionary.Add("basic straight road", createBasicStraightTileset());
+        roadTilesSetDictionary.Add("basic straight road2", createBasicStraightTileset());
         roadTilesSetDictionary.Add("single curve straight road", createSingleCurveStraightTileset());
         roadTilesSetDictionary.Add("single left curve straight road", createSingleLeftCurveStraightTileset());
         roadTilesSetDictionary.Add("single diagonal straight road", createSingleDiagonalTileset());
+        roadTilesSetDictionary.Add("single left diagonal straight road", createSingleLeftDiagonalTileset());
+        roadTilesSetDictionary.Add("single diagonal straight road2", createSingleDiagonalTileset());
+        roadTilesSetDictionary.Add("single left diagonal straight road2", createSingleLeftDiagonalTileset());
+        
+        roadTilesSetDictionary["basic straight road"].transform.position = new Vector3(initialX, -21f, 0.2f);
+        roadTilesSetDictionary["single curve straight road"].transform.position = new Vector3(initialX, 0f, 0.2f);
+        roadTilesSetDictionary["single left diagonal straight road"].transform.position = new Vector3(initialX, 21f, 0.2f);
+        roadTilesSetDictionary["basic straight road2"].transform.position = new Vector3(initialX, 42f, 0.2f);
+        roadTilesSetDictionary["single diagonal straight road"].transform.position = new Vector3(initialX, 63f, 0.2f);
+        roadTilesSetDictionary["single left curve straight road"].transform.position = new Vector3(initialX, 84f, 0.2f);
+        roadTilesSetDictionary["single diagonal straight road2"].transform.position = new Vector3(initialX, 105f, 0.2f);
+        roadTilesSetDictionary["single left diagonal straight road2"].transform.position = new Vector3(initialX, 126f, 0.2f);
 
-        roadTilesSetDictionary["single curve straight road"].transform.position = new Vector3(-6f, 0f, 0.2f);
-        roadTilesSetDictionary["single left curve straight road"].transform.position = new Vector3(-6f, -21f, 0.2f);
+        allTilesets = new GameObject[8];
 
-        roadTilesSetDictionary["single diagonal straight road"].transform.position = new Vector3(-6f, 42f, 0.2f);
-        roadTilesSetDictionary["basic straight road"].transform.position = new Vector3(-6f, 21f, 0.2f);
+        allTilesets[0] = roadTilesSetDictionary["basic straight road"];
+        allTilesets[1] = roadTilesSetDictionary["single curve straight road"];
+        allTilesets[2] = roadTilesSetDictionary["single left diagonal straight road"];
+        allTilesets[3] = roadTilesSetDictionary["basic straight road2"];
+        allTilesets[4] = roadTilesSetDictionary["single diagonal straight road"];
+        allTilesets[5] = roadTilesSetDictionary["single left curve straight road"];
+        allTilesets[6] = roadTilesSetDictionary["single diagonal straight road2"];
+        allTilesets[7] = roadTilesSetDictionary["single left diagonal straight road2"];
+    }
+
+    void FixedUpdate()
+    {
+        updateBaseTilesPosition();
+    }
+
+    public void updateBaseTilesPosition()
+    {
+        if(charactersManager.getMainPlayerTransform() != null)
+        {
+            float characterX = charactersManager.getMainPlayerTransform().position.x;
+            float characterY = charactersManager.getMainPlayerTransform().position.y;
+
+            for(int i=0; i < tileSetsAmount; i++)
+            {
+                Vector3 position = allTilesets[i].transform.position;
+                float distance = Mathf.Abs(position.y - characterY);
+                float maxDist = (completeTileSetSize * tileSetsAmount) / 2;
+                if(distance > maxDist) 
+                {
+                    //Debug.Log("moving");
+                    float newY = 0;
+                    if (position.y > characterY)
+                    {
+                        newY = position.y - completeTileSetSize * tileSetsAmount;
+                    }
+                    else
+                    {
+                        newY = position.y + completeTileSetSize * tileSetsAmount;
+                    }
+                    allTilesets[i].transform.position = new Vector3(position.x, newY, position.z);
+                }
+                /*
+                else
+                {
+                    Debug.Log("OK, " + distance + " <= " + maxDist);
+                }*/
+            }
+        }
+        else
+        {
+            Debug.Log("no player transform");
+        }
     }
 
     public GameObject createSingleDiagonalTileset()
@@ -51,6 +122,21 @@ public class MapTilesManager : MonoBehaviour {
         return output;
     }
 
+    public GameObject createSingleLeftDiagonalTileset()
+    {
+        GameObject output = instantiateRoadTileset();
+        for(int y = 0; y < 3; y++)
+        {
+            addTileToSet(output, instantiateTileByName("single road"), 4.5f, y + 0.5f, false, false, DirectionConstants.SUR_NORTE);
+        }
+        addTileToSet(output, instantiateTileByName("diagonal 2x2 road"), 4f, 4f, false, true, DirectionConstants.SUR_NORTE);
+        for(int y = 5; y < 7; y++)
+        {
+            addTileToSet(output, instantiateTileByName("single road"), 3.5f, y + 0.5f, false, false, DirectionConstants.SUR_NORTE);
+        }
+
+        return output;
+    }
 
     public GameObject createSingleCurveStraightTileset()
     {
@@ -105,8 +191,6 @@ public class MapTilesManager : MonoBehaviour {
         }
         return output;
     }
-
-
 
     public void addTileToSet(GameObject tileSet, GameObject tile, float positionX, float positionY, bool flippedX, bool flippedY, string direction)
     {
