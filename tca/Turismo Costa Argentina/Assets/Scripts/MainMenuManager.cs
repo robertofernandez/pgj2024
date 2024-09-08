a9d9677a7bc63562f09758e30f770e9c9cbbd4ad
Roberto G. Fernández <rgfernan@gmail.com>
2024-09-08 15:53:49 -0300

    Menu de seleccion de playas.

--- tca/Turismo Costa Argentina/Assets/Scripts/MainMenuManager.cs
+++ tca/Turismo Costa Argentina/Assets/Scripts/MainMenuManager.cs
@@ -10,6 +10,20 @@ public class MainMenuManager : MonoBehaviour
 
     public void QuitGame()
     {
+        Debug.Log("Quitting game");
         Application.Quit();
     }
+
+    public void GotoPuertoMadryn()
+    {
+        //TODO setup Puerto Madryn configuration
+        SceneManager.LoadScene(1);
+    }
+
+    public void GotoPuntaMedanos()
+    {
+        //TODO setup Punta Medanos configuration
+        SceneManager.LoadScene(1);
+    }
+
 }
